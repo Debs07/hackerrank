9a207ae459fb67796f995807cd3b2b5c17002871
Debs07 <magidebora@gmail.com>
2020-03-11 11:17:37 +0000

    Python If Else solution

---
+++ python-if-else/solution.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+"""
+Given an integer, n, perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+"""
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1:
+        print("Weird")
+        exit()
+
+    if n in range(2,6) or n > 20:
+        print("Not Weird")
+        exit() 
+    
+    if n in range(6,21):
+        print("Weird")
+        exit()
+
+
