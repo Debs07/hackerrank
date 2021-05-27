b9448756e0876320be10dffb4f1c826e1e41946e
Debs07 <59568465+Debs07@users.noreply.github.com>
2021-05-27 15:47:43 +0100

    Update solution.py

--- find-the-runner-up-score/solution.py
+++ find-the-runner-up-score/solution.py
@@ -1,19 +1,9 @@
-def func_maximo():
-    maximo = Lista[0]
-    for z in range(len(Lista)):
-        if Lista[z] > maximo:
-            maximo = Lista[z]
-    return maximo
-
-
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    Lista = []
-    for x in arr:
-        Lista.append(x)
-    x = func_maximo()
-    while func_maximo() == x:
-        Lista.remove(x)
-    print(func_maximo())
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    x = max(arr)
+    while max(arr) == x:
+        arr.remove(max(arr))
+print(max(arr)) 
+    
 
