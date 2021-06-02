666f29a093d9a512e7bfbc2a5f0cd026b8d85046
Debs07 <59568465+Debs07@users.noreply.github.com>
2021-06-02 16:58:41 +0100

    Update solution.py

--- finding-the-percentage/solution.py
+++ finding-the-percentage/solution.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     n = int(raw_input())
     student_marks = {}
-    for _ in range(n):
+    for i in range(n):
         line = raw_input().split()
         name, scores = line[0], line[1:]
         scores = map(float, scores)
